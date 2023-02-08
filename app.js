c481f58632a8ea089766ebb2a325ee1a52621905
NoaED05 <89384574+NoaED05@users.noreply.github.com>
2023-02-08 14:20:19 +0100

    commit test

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const sqlite3 = require('better-sqlite3')
+const db = sqlite3('coockie-clicker.db', {verbose:console.log})
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+app.listen("3000", () => {
+    console.log("UP!")
+})
