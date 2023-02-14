067a90fa777b9475109f7dd78d5e983837755c8c
NoaED05 <noaed05@outlook.com>
2023-02-14 08:34:08 +0100

    llllll

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 const express = require("express");
 const sqlite3 = require('better-sqlite3')
-const db = sqlite3('team.db', {verbose:console.log})
+const db = sqlite3('coockie-clicker.db', {verbose:console.log})
 const path = require("path")
+const session = require('express-session')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, "/public")))
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: "Keep it secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
 app.post("/addUser", (request, response) => {
-    const sql = db.prepare('INSERT INTO user (username,password,points) VALUES (?,?,?)')
+    const sql = db.prepare('INSERT INTO User (username,password,points  ) VALUES (?,?,?)')
     
     const info = sql.run(request.body.username, request.body.password, request.body.points)
     console.log("Amount changes done: " + info.changes)
     console.log("lastInsertRowID: " + info.lastInsertRowID)
     
   
-    response.redirect("http://localhost:3000/")
+    response.redirect("/")
 })
 
 app.post("/login", (request, response) => {
-    const sql = db.prepare("SELECT * FROM user WHERE username=(?)")
+    const sql = db.prepare("SELECT * FROM user WHERE username =(?)")
     const results = sql.get(request.body.username)
-    if (request.body.password === results.password){
-       response.redirect("http://localhost:3000/")
+    if (request.body.password == results.password){
+        request.session.logedIn = true
+        response.redirect("/")
     }
     else {
         console.log("shit")
+        request.session.logedIn = false
         response.redirect("back")
     }
-
+  
+    
 })
 
 
